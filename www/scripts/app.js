f7984bd77cd604ceb0a83a2146943124396de4dd
adrion <frossardcedric@gmail.com>
2016-04-21 12:59:06 +0200

    Ishihara game translations

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -8,10 +8,28 @@ app.init = function () {
 app.config(['$translateProvider', function($translateProvider) {
   $translateProvider.translations('en', {
     "KEY": "Value",
+    "BACK": "Back",
+    "NEXT": "Next",
+    "START": "Start",
+    "RESTART": "Restart",
+    "RESULTS": "Results",
+    "BACK_TO_MENU": "Back to menu",
+    "EXPECTED": "Expected",
+    "YOUR_ANSWER": "Your answer",
+    "YOUR_DIAGNOSTIC": "Your diagnostic",
     "DIAG_ISHIHARA_NORMAL": "normal vision",
   });
   $translateProvider.translations('fr', {
     "KEY": "Valeur",
+    "BACK": "Retour",
+    "NEXT": "Suivant",
+    "RESTART": "Recommencer",
+    "START": "Commencer",
+    "RESULTS": "Résultats",
+    "BACK_TO_MENU": "Retour au menu",
+    "EXPECTED": "Attendu",
+    "YOUR_ANSWER": "Votre réponse",
+    "YOUR_DIAGNOSTIC": "Votre diagnostic",
     "DIAG_ISHIHARA_NORMAL": "vision normale",
   });
 
