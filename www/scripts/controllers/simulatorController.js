2f3e0447fc2fde72313a5c8874c0c168423565c7
cedric frossard <adrion.dev@gmail.com>
2016-04-22 12:23:06 +0200

    Vision simulator getUserMedia and toggle on/off

--- www/scripts/controllers/simulatorController.js
+++ www/scripts/controllers/simulatorController.js
@@ -1,3 +1,42 @@
-angular.module('app').controller('SimulatorController', function ($scope) {
+app.controller('simulatorController', function ($scope) {
   console.log('SimulatorController');
+
+  $scope.video = document.querySelector('video');
+
+  function successCallback(stream) {
+    console.log('success');
+    $scope.video.src = window.URL.createObjectURL(stream);
+
+    navigatorMain.on('postpop', function(event){
+      $scope.video.pause();
+      $scope.video.src = '';
+      stream.getVideoTracks()[0].stop();
+    });
+  }
+
+  function errorCallback(error) {
+    console.log('navigator.getUserMedia error: ', error);
+  }
+
+  function onDeviceReady() {
+
+    if (typeof MediaStreamTrack === 'undefined' ||
+      typeof MediaStreamTrack.getSources === 'undefined') {
+      alert('This browser does not support MediaStreamTrack.\n\nTry Chrome.');
+    } else {
+      $scope.source = MediaStreamTrack.getSources(function(sources){
+        navigator.webkitGetUserMedia({
+          audio:false,
+          video: {
+            optional: [{
+              sourceId: sources[2].id || sources[1].id || sources[0].id
+            }]
+          }
+        }, successCallback, errorCallback);
+      });
+    }
+  }
+
+  document.addEventListener('deviceready', onDeviceReady, false);
+  onDeviceReady();
 });
