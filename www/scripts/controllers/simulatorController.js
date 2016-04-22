38bf52fcf5995609a0242f72434ce2ccd78ac887
cedric frossard <adrion.dev@gmail.com>
2016-04-22 13:17:43 +0200

    Vision simulator dynamic video size

--- www/scripts/controllers/simulatorController.js
+++ www/scripts/controllers/simulatorController.js
@@ -2,16 +2,24 @@ app.controller('simulatorController', function ($scope) {
   console.log('SimulatorController');
 
   $scope.video = document.querySelector('video');
+  console.log(document.getElementsByClassName('.navigation-bar__title'));
+  window.addEventListener('resize', resizeVideo);
 
   function successCallback(stream) {
-    console.log('success');
     $scope.video.src = window.URL.createObjectURL(stream);
+    resizeVideo();
 
     navigatorMain.on('postpop', function(event){
       $scope.video.pause();
       $scope.video.src = '';
       stream.getVideoTracks()[0].stop();
     });
+  }
+
+  function resizeVideo() {
+    console.log("resize");
+    $scope.video.height = window.innerHeight - 44;
+    $scope.video.width = window.innerWidth;
   }
 
   function errorCallback(error) {
@@ -24,13 +32,13 @@ app.controller('simulatorController', function ($scope) {
       typeof MediaStreamTrack.getSources === 'undefined') {
       alert('This browser does not support MediaStreamTrack.\n\nTry Chrome.');
     } else {
+
+
       $scope.source = MediaStreamTrack.getSources(function(sources){
         navigator.webkitGetUserMedia({
           audio:false,
           video: {
-            optional: [{
-              sourceId: sources[2].id || sources[1].id || sources[0].id
-            }]
+            optional: [{ sourceId: sources[2].id || sources[1].id || sources[0].id }]
           }
         }, successCallback, errorCallback);
       });
