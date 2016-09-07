a633ce0a65411f1f1733ae3c6fd0f8d98747aca0
cedric frossard <adrion.dev@gmail.com>
2016-09-07 13:59:00 +0200

    add temporary static HON about content

--- www/scripts/controllers/aboutController.js
+++ www/scripts/controllers/aboutController.js
@@ -1,3 +1,7 @@
-angular.module('app').controller('AboutController', function() {
+angular.module('app').controller('AboutController', function($scope) {
   console.log('AboutController');
+  $scope.openExternalLink = function() {
+    window.open('https://hon.ch/20-years/fr/a-propos.html',
+      '_system', 'location=yes');
+  };
 });
