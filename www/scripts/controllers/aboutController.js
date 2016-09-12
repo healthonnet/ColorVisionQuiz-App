79edba69f4e49d365211e0154ae38b94b96d1f9a
cedric frossard <adrion.dev@gmail.com>
2016-09-12 09:09:43 +0200

    update fr - about page

--- www/scripts/controllers/aboutController.js
+++ www/scripts/controllers/aboutController.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('AboutController', function($scope) {
   console.log('AboutController');
-  $scope.openExternalLink = function() {
-    window.open('https://hon.ch/20-years/fr/a-propos.html',
+  $scope.openExternalLink = function(link) {
+    window.open(link,
       '_system', 'location=yes');
   };
 });
