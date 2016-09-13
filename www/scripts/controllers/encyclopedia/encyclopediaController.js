a7fcf3a4a3660f8530c0d50ae32c65e9bcca55a7
cedric frossard <adrion.dev@gmail.com>
2016-09-13 10:49:25 +0200

    clear controllers

--- www/scripts/controllers/encyclopedia/encyclopediaController.js
+++ www/scripts/controllers/encyclopedia/encyclopediaController.js
@@ -1,23 +1,5 @@
 angular.module('app')
-  .controller('EncyclopediaController', function($rootScope) {
+  .controller('EncyclopediaController', function() {
   console.log('EncyclopediaController');
 
-  // TODO get article list on init. Dist json ?
-  $rootScope.topics = [
-    {
-      id: 1,
-      label: 'a topic',
-      description: 'description topic',
-      image: 'assets/articles/color.jpg',
-      date: '2016-08-25',
-      author: 'me',
-    },
-    {
-      id: 2,
-      label: '2 a topic',
-      description: '2 description topic',
-      date: '2016-08-26',
-      author: 'me',
-    },
-  ];
 });
