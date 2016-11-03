c9c07d8fd5c6edcb65daab1f7a0dc8d1d9634f7c
cedric frossard <adrion.dev@gmail.com>
2016-11-03 12:29:13 +0100

    Add IshiharaTestController tests

--- spec/controllers/ishihara/ishiharaTestController.spec.js
+++ spec/controllers/ishihara/ishiharaTestController.spec.js
@@ -29,12 +29,11 @@ describe('ishiharaTestController', function() {
   describe('$scope.nextQuestion', function() {
     it('should increment currentQuestion', function() {
       var $scope = {
-        currentQuestion: 1
+        currentQuestion: 1,
       };
       var $http = {};
 
-      var controller =
-        $controller('ishiharaTestController', { $scope: $scope, $http: $http});
+      $controller('ishiharaTestController', { $scope: $scope, $http: $http});
 
       $scope.nextQuestion();
 
@@ -46,12 +45,11 @@ describe('ishiharaTestController', function() {
     it('should push answer to answers', function() {
       var $scope = {
         currentQuestion: 1,
-        answers: ['test']
+        answers: ['test'],
       };
       var $http = {};
 
-      var controller =
-        $controller('ishiharaTestController', { $scope: $scope, $http: $http});
+      $controller('ishiharaTestController', { $scope: $scope, $http: $http});
 
       $scope.answer('test2');
 
@@ -60,4 +58,39 @@ describe('ishiharaTestController', function() {
     });
   });
 
+  describe('$scope.generateQuiz', function() {
+    it('should generate short Quiz', function() {
+      var $scope = {
+        currentQuestion: 1,
+        short: true,
+      };
+      var $http = {};
+      var data = {
+        plates: [0,1,2,3,4,5,
+        6,7,8,9,10,11,12,13,
+        14,15,16,17,18,19,20,],
+      };
+      $controller('ishiharaTestController', { $scope: $scope, $http: $http});
+
+      var quiz = $scope.generateQuiz(data);
+      expect(quiz.length).toEqual(6);
+    });
+    it('should generate long Quiz', function() {
+      var $scope = {
+        currentQuestion: 1,
+        short: false,
+      };
+      var $http = {};
+      var data = {
+        plates: [1,2,3,4,5,
+          6,7,8,9,10,11,12,13,
+          14,15,16,17,18,19,20,
+          21,22,23,24,25,],
+      };
+      $controller('ishiharaTestController', { $scope: $scope, $http: $http});
+
+      var quiz = $scope.generateQuiz(data);
+      expect(quiz.length).toEqual(25);
+    });
+  });
 });
