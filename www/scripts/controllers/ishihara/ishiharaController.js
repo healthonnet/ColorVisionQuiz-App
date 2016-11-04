bb0b5282c5c87c65dee9d7a064b213f4079487bb
William Belle <william.belle@gmail.com>
2016-11-04 12:30:37 +0100

    add ishihara modal instruction

--- www/scripts/controllers/ishihara/ishiharaController.js
+++ www/scripts/controllers/ishihara/ishiharaController.js
@@ -2,5 +2,8 @@ angular.module('app').controller('ishiharaController', function($scope) {
   console.log('IshiharaController');
   $scope.init = function() {
     $scope.short = true;
+    $scope.show = function() {
+      modalIshiharaTest.show();
+    };
   };
 });
