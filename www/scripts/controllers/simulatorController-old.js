0c27d30956f8c2763333cd7cc42eb48f8dbf692d
cedric frossard <adrion.dev@gmail.com>
2018-08-09 13:47:39 +0200

    Related to #12 - Static and cross platform alternative for simulator

---
+++ www/scripts/controllers/simulatorController-old.js
@@ -0,0 +1,143 @@
+app.controller('simulatorController', function($scope, $translate) {
+  console.log('SimulatorController');
+  var that = this;
+
+  this.resizeVideo = function() {
+    $scope.videoRight.height = window.innerHeight - 44;
+    $scope.video.height = window.innerHeight - 44;
+    $scope.videoRight.width = window.innerWidth;
+    $scope.video.width = window.innerWidth;
+  };
+
+  this.switchOnAR = function() {
+    try {
+      $scope.videoRight.srcObject = $scope.stream;
+    } catch (error) {
+      $scope.videoRight.src = $scope.video.src;
+    }
+    $scope.videoRight.play();
+    $scope.videoRight.className = $scope.video.className;
+  };
+
+  this.switchOffAR = function() {
+    $scope.videoRight.src = '';
+    $scope.videoRight.pause();
+    $scope.videoRight.className = '';
+  };
+
+  this.stopVideo = function() {
+    $scope.video.pause();
+    $scope.videoRight.pause();
+    $scope.video.src = '';
+    $scope.videoRight.src = '';
+
+    if ($scope.stream) {
+      $scope.stream.getVideoTracks()[0].stop();
+    }
+  };
+
+  function successCallback(stream) {
+    $scope.stream = stream;
+
+    try {
+      $scope.video.srcObject = stream;
+    } catch (error) {
+      $scope.video.src = window.URL.createObjectURL(stream);
+    }
+    that.resizeVideo();
+
+    navigatorMain.on('prepush', that.stopVideo);
+    navigatorMain.on('postpop',that.stopVideo);
+
+  }
+
+  $scope.staticVideoFallback = function() {
+    $scope.video.src = 'assets/video/paint.mp4';
+    $scope.video.loop = true;
+    navigatorMain.on('prepush', that.stopVideo);
+    navigatorMain.on('postpop',that.stopVideo);
+  };
+
+  function errorCallback(error) {
+    console.log('navigator.getUserMedia error: ', error);
+    $scope.staticVideoFallback();
+  }
+
+  // Export functions
+  $scope.updateFilter = function() {
+    if ($scope.stopTalking) {
+      $scope.stopTalking();
+    }
+    var prefix = '';
+    var filter = $scope.selectedFilter.toLowerCase() + 'Effect';
+    if ($scope.armode) {
+      prefix += 'stereo ';
+      $scope.videoRight.className = prefix + filter;
+    }
+    $scope.video.className = prefix + filter;
+  };
+
+  $scope.toggleAR = function() {
+    $scope.armode = !$scope.armode;
+    if ($scope.armode) {
+      that.switchOnAR();
+    } else {
+      that.switchOffAR();
+    }
+  };
+
+  $scope.show = function() {
+    modalSimulator.show();
+  };
+
+  $scope.readDisease = function(disease) {
+    if (!disease) {
+      return;
+    }
+    var lang = $translate.use() === 'fr' ? 'fr-FR' : 'en-GB';
+    $translate(disease + '_AUDIO').then(function(translation) {
+      $scope.talk({
+        text: translation,
+        locale: lang,
+      });
+    });
+  };
+
+  // Init
+  $scope.init = function() {
+    $scope.selectedFilter = 'NORMAL';
+    $scope.video = document.querySelector('video#left');
+    $scope.videoRight = document.querySelector('video#right');
+    $scope.static = true;
+
+    // Events
+    window.addEventListener('resize', that.resizeVideo);
+    ons.orientation.on('change', function() {
+      if (ons.orientation.isPortrait() && $scope.armode) {
+        $scope.armode = false;
+        that.switchOffAR();
+        $scope.$apply();
+      }
+    });
+    navigatorMain.on('prepush', $scope.stopTalking);
+    navigatorMain.on('prepop', $scope.stopTalking);
+
+    // Load media stream
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({
+        video: { facingMode: { exact: 'environment'}},
+      })
+        .then(function(stream) {
+          $scope.static = false;
+          successCallback(stream);
+        })
+        .catch(function(error) {
+          errorCallback(error);
+        });
+    } else {
+      // Load static video instead
+      $scope.staticVideoFallback();
+    }
+  };
+
+});
--- www/scripts/controllers/simulatorController.js
+++ www/scripts/controllers/simulatorController.js
@@ -1,67 +1,10 @@
 app.controller('simulatorController', function($scope, $translate) {
   console.log('SimulatorController');
-  var that = this;
 
-  this.resizeVideo = function() {
-    $scope.videoRight.height = window.innerHeight - 44;
-    $scope.video.height = window.innerHeight - 44;
-    $scope.videoRight.width = window.innerWidth;
-    $scope.video.width = window.innerWidth;
+  $scope.resizeVideo = function() {
+    $scope.slider.height = window.innerHeight - 44;
+    console.log($scope.slider.height);
   };
-
-  this.switchOnAR = function() {
-    try {
-      $scope.videoRight.srcObject = $scope.stream;
-    } catch (error) {
-      $scope.videoRight.src = $scope.video.src;
-    }
-    $scope.videoRight.play();
-    $scope.videoRight.className = $scope.video.className;
-  };
-
-  this.switchOffAR = function() {
-    $scope.videoRight.src = '';
-    $scope.videoRight.pause();
-    $scope.videoRight.className = '';
-  };
-
-  this.stopVideo = function() {
-    $scope.video.pause();
-    $scope.videoRight.pause();
-    $scope.video.src = '';
-    $scope.videoRight.src = '';
-
-    if ($scope.stream) {
-      $scope.stream.getVideoTracks()[0].stop();
-    }
-  };
-
-  function successCallback(stream) {
-    $scope.stream = stream;
-
-    try {
-      $scope.video.srcObject = stream;
-    } catch (error) {
-      $scope.video.src = window.URL.createObjectURL(stream);
-    }
-    that.resizeVideo();
-
-    navigatorMain.on('prepush', that.stopVideo);
-    navigatorMain.on('postpop',that.stopVideo);
-
-  }
-
-  $scope.staticVideoFallback = function() {
-    $scope.video.src = 'assets/video/paint.mp4';
-    $scope.video.loop = true;
-    navigatorMain.on('prepush', that.stopVideo);
-    navigatorMain.on('postpop',that.stopVideo);
-  };
-
-  function errorCallback(error) {
-    console.log('navigator.getUserMedia error: ', error);
-    $scope.staticVideoFallback();
-  }
 
   // Export functions
   $scope.updateFilter = function() {
@@ -70,20 +13,7 @@ app.controller('simulatorController', function($scope, $translate) {
     }
     var prefix = '';
     var filter = $scope.selectedFilter.toLowerCase() + 'Effect';
-    if ($scope.armode) {
-      prefix += 'stereo ';
-      $scope.videoRight.className = prefix + filter;
-    }
-    $scope.video.className = prefix + filter;
-  };
-
-  $scope.toggleAR = function() {
-    $scope.armode = !$scope.armode;
-    if ($scope.armode) {
-      that.switchOnAR();
-    } else {
-      that.switchOffAR();
-    }
+    $scope.slider.className = prefix + filter;
   };
 
   $scope.show = function() {
@@ -103,41 +33,36 @@ app.controller('simulatorController', function($scope, $translate) {
     });
   };
 
+  $scope.imgLibrary = [
+    './assets/images/1.jpg',
+    './assets/images/2.jpg',
+  ];
+
+  $scope.nextImg = function() {
+    if ($scope.fakeslider === ($scope.imgLibrary.length - 1)) {
+      $scope.fakeslider = 0;
+    } else {
+      $scope.fakeslider++;
+    }
+  };
+  $scope.prevImg = function() {
+    if ($scope.fakeslider === 0) {
+      $scope.fakeslider = $scope.imgLibrary.length - 1;
+    } else {
+      $scope.fakeslider--;
+    }
+  };
+
   // Init
   $scope.init = function() {
     $scope.selectedFilter = 'NORMAL';
-    $scope.video = document.querySelector('video#left');
-    $scope.videoRight = document.querySelector('video#right');
-    $scope.static = true;
+    $scope.slider = document.querySelector('#slider');
+    $scope.fakeslider = 0;
+    $scope.resizeVideo();
 
-    // Events
-    window.addEventListener('resize', that.resizeVideo);
-    ons.orientation.on('change', function() {
-      if (ons.orientation.isPortrait() && $scope.armode) {
-        $scope.armode = false;
-        that.switchOffAR();
-        $scope.$apply();
-      }
-    });
+    ons.orientation.on('change',$scope.resizeVideo);
     navigatorMain.on('prepush', $scope.stopTalking);
     navigatorMain.on('prepop', $scope.stopTalking);
-
-    // Load media stream
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({
-        video: { facingMode: { exact: 'environment'}},
-      })
-        .then(function(stream) {
-          $scope.static = false;
-          successCallback(stream);
-        })
-        .catch(function(error) {
-          errorCallback(error);
-        });
-    } else {
-      // Load static video instead
-      $scope.staticVideoFallback();
-    }
   };
 
 });
